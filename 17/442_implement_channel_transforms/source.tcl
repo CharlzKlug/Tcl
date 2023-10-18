7f1ae73b020622b8fc385fd9a79feac0b0565b6f
Imil Ametov <charlz_klug@mail.ru>
2023-10-18 10:21:55 +0300

    Added Transform

--- 17/442_implement_channel_transforms/source.tcl
+++ 17/442_implement_channel_transforms/source.tcl
@@ -1,5 +1,4 @@
 package require rc4
-
 oo::class create RC4Transform {
     variable RC4Read
     variable RC4Write
@@ -7,10 +6,18 @@ oo::class create RC4Transform {
 	set RC4Read [rc4::RC4Init $key]
 	set RC4Write [rc4::RC4Init $key]
     }
-}
-
-oo::define RC4Transform {
     method initialize {transform_handle mode} {
 	return {initialize read write finalize}
     }
+method finalize {transform_handle} {
+    rc4::RC4Final $RC4Read
+    rc4::RC4Final $RC4Write
+    [self] destroy
+}
+method read {transform_handle bytes} {
+    return [rc4::RC4 $RC4Read $bytes]
+}
+method write {transform_handle bytes} {
+    return [rc4::RC4 $RC4Write $bytes]
+}
 }
