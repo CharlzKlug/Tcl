c4459e62aa47477c8a81da2f95c6be182fa11d11
Ametov Imil <charlz_klug@mail.ru>
2023-10-19 22:52:42 +0300

    Indentation for transforms

--- 17/442_implement_channel_transforms/source.tcl
+++ 17/442_implement_channel_transforms/source.tcl
@@ -9,15 +9,15 @@ oo::class create RC4Transform {
     method initialize {transform_handle mode} {
 	return {initialize read write finalize}
     }
-method finalize {transform_handle} {
-    rc4::RC4Final $RC4Read
-    rc4::RC4Final $RC4Write
-    [self] destroy
-}
-method read {transform_handle bytes} {
-    return [rc4::RC4 $RC4Read $bytes]
-}
-method write {transform_handle bytes} {
-    return [rc4::RC4 $RC4Write $bytes]
-}
+    method finalize {transform_handle} {
+	rc4::RC4Final $RC4Read
+	rc4::RC4Final $RC4Write
+	[self] destroy
+    }
+    method read {transform_handle bytes} {
+	return [rc4::RC4 $RC4Read $bytes]
+    }
+    method write {transform_handle bytes} {
+	return [rc4::RC4 $RC4Write $bytes]
+    }
 }
