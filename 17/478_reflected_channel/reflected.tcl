aaaef504c24b049b44a4f4a13219a94d4919ff8a
Ametov Imil <charlz_klug@mail.ru>
2023-10-25 00:03:33 +0300

    Notify bureaucrat

--- 17/478_reflected_channel/reflected.tcl
+++ 17/478_reflected_channel/reflected.tcl
@@ -43,3 +43,36 @@ proc ::bureaucrat::cgetall {chan} {
     return [list -delay [dict get $channels $chan Delay]]
 }
 	
+proc ::bureaucrat::blocking {chan mode} {
+    variable channels
+    dict set channels($chan) Blocking $mode
+}
+
+proc ::bureaucrat::watch {chan events} {
+    variable channels
+    set watched [dict get $channels($chan) Watch]
+    dict set channels($chan) Watch $events
+    if {"read" in $events && "read" ni $watched} {
+	notify $chan read
+    }
+    if {"write" in $events && "write" ni $watched} {
+	notify $chan write
+    }
+}
+
+proc ::bureaucrat::notify {chan event} {
+    variable channels
+    dict with channels($chan) {
+	if {$event ni $Watch} {
+	    return
+	}
+	if {$event eq "read"} {
+	    if {[string length $Data] == 0 && $State ne "EOF"} {
+		return
+	    }
+	} else {
+	    if {$State ne "OPEN"} return
+	}
+    }
+    after idle [list after 0 [list chan postevent $chan $event]]
+}
