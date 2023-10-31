f1568b63767b93a1f3fb170fbc380720f9c343c7
Imil Ametov <charlz_klug@mail.ru>
2023-10-31 11:32:24 +0300

    Fixed typo in async example

--- 18/467_async/source.tcl
+++ 18/467_async/source.tcl
@@ -1,6 +1,6 @@
 proc on_read so {
     set s [read $so]
-    if {[string length $so] == 0} {
+    if {[string length $s] == 0} {
 	if {[eof $so]} {
 	    close $so
 	    set ::done 1
@@ -11,13 +11,13 @@ proc on_read so {
 }
 
 proc on_write so {
-    puts $so "GET http://www.example.com HTTP/1.1\n"
+    puts $so "GET / HTTP/1.1\nHost:httpforever.com\n\n"
     chan event $so writable {}
     chan close $so write
 }
 
-# set so [socket -async www.example.com 80]
-# fconfigure $so -blocking 0 -encoding utf-8 -translation crlf -buffering line
-# chan event $so readable [list on_read $so]
-# chan event $so writable [list on_write $so]
-# vwait ::done
+set so [socket -async httpforever.com 80]
+fconfigure $so -blocking 0 -encoding utf-8 -translation crlf -buffering line
+chan event $so readable [list on_read $so]
+chan event $so writable [list on_write $so]
+vwait ::done
