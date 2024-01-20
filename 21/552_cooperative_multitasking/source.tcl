4baa4882fb267e4ea4d3416e32027b69b1787756
Ametov Imil <charlz_klug@mail.ru>
2024-01-20 03:07:40 +0300

    Chapter 21. Cooperative multitasking

---
+++ 21/552_cooperative_multitasking/source.tcl
@@ -0,0 +1,30 @@
+package require coroutine
+namespace eval task {
+    variable tasks {}
+}
+
+proc task::task {script} {
+    variable next_id
+    variable tasks
+    variable tasks_added
+
+    set task_id [namespace current]::task#[incr next_id]
+    dict set tasks $task_id State INIT
+    dict set tasks $task_id Messages {}
+    set tasks_added 1
+    coroutine $task_id apply {script {
+	yield
+	try $script finally {task::cleanup [task::myself]}
+    }} $script
+    wakeup_dispatcher
+    return $task_id
+}
+
+proc task::myself {} {
+    variable tasks
+    set me [info coroutine]
+    if {[dict exists $tasks $me]} {
+	return $me
+    }
+    error "Not running in a task."
+}
